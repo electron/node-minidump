41b0b1b2049c90e74cf632685774cae113f885e8
Jeremy Apthorp <nornagon@nornagon.net>
2019-06-27 12:12:26 -0700

    build dump_syms

---
+++ build.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const path = require('path')
+const {spawnSync} = require('child_process')
+
+const buildDir = path.join(__dirname, 'build')
+if (!fs.existsSync(buildDir)) {
+  fs.mkdirSync(buildDir, {recursive: true})
+}
+spawnSync(path.join(__dirname, 'deps', 'breakpad', 'configure'), [], {
+  cwd: buildDir,
+  env: {
+    ...process.env,
+    CPPFLAGS: `-I${path.relative(buildDir, path.join(__dirname, 'deps'))}`
+  },
+  stdio: 'inherit'
+})
+const targets = ['src/processor/minidump_stackwalk']
+if (process.platform === 'linux') {
+  targets.push('src/tools/linux/dump_syms/dump_syms')
+} else if (process.platform === 'darwin') {
+  targets.push('src/tools/mac/dump_syms/dump_syms_mac')
+}
+spawnSync('make', ['-C', buildDir, '-j', require('os').cpus().length, ...targets], {
+  stdio: 'inherit'
+})
