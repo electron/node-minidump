a0583179357d9cb8f454b3937cb25415b08a8432
Amin Yahyaabaadi <aminyahyaabadi74@gmail.com>
2020-12-15 12:23:11 -0600

    fix: add includes needed for mac target

--- build.js
+++ build.js
@@ -6,11 +6,18 @@ const buildDir = path.join(__dirname, 'build')
 if (!fs.existsSync(buildDir)) {
   fs.mkdirSync(buildDir, { recursive: true })
 }
+
+let includes = `-I${path.relative(buildDir, path.join(__dirname, 'deps'))}`
+if (process.platform === 'darwin') {
+  // needed for `#include "common/*.h"` in `src/tools/mac/dump_syms/dump_syms_tool`
+  includes = includes + ` -I${path.relative(buildDir, path.join(__dirname, 'deps', 'breakpad', 'src'))}`
+}
+
 spawnSync(path.join(__dirname, 'deps', 'breakpad', 'configure'), [], {
   cwd: buildDir,
   env: {
     ...process.env,
-    CPPFLAGS: `-I${path.relative(buildDir, path.join(__dirname, 'deps'))}`
+    CPPFLAGS: includes
   },
   stdio: 'inherit'
 })
@@ -22,6 +29,6 @@ if (process.platform === 'darwin') {
   targets.push('src/tools/mac/dump_syms/dump_syms_tool')
 }
 
-spawnSync('make', ['-C', buildDir, '-j', require('os').cpus().length, ...targets], {
+spawnSync('make', [includes, '-C', buildDir, '-j', require('os').cpus().length, ...targets], {
   stdio: 'inherit'
 })
